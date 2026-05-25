cd94a14aa89753a8b3107e47df9a84c35f1ebe28
grandeemir <emircanozkara10@gmail.com>
2026-05-25 12:09:32 +0300

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // MySQL Connection Pool
 const pool = mysql.createPool({
